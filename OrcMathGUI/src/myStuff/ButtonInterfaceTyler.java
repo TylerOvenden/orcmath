224fb4b49f00c378a87244769c16478a45736650
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-08 10:31:16 -0500

    dsc

--- OrcMathGUI/src/myStuff/ButtonInterfaceTyler.java
+++ OrcMathGUI/src/myStuff/ButtonInterfaceTyler.java
@@ -1,5 +1,8 @@
 package myStuff;
 
+import java.awt.Color;
+
+import guiTeacher.components.Action;
 import guiTeacher.interfaces.Clickable;
 public interface ButtonInterfaceTyler extends Clickable {
 	void setColor(Color color);
